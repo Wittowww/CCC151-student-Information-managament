3067bdc2d83f4b33399d8a4cd5e6274a8087a72e
Niezsa.Bacus <bacusjuniezsa@gmail.com>
2026-02-22 19:38:21 +0800

    creating folders to know which one to do ;((

--- Student Information Management/GUI/WindowMain.py
+++ Student Information Management/GUI/WindowMain.py
@@ -3,23 +3,31 @@ from PySide6.QtGui import QIcon
 
 
 class mainApp(QMainWindow):
-    def __init__(self, app):
+    def __init__(self):
         super().__init__()
-        self.app = app
         self.setWindowTitle("Student Information Management System")
         self.setFixedSize(1200, 700)
         self.setWindowIcon(QIcon("stuinfo ICON.png"))
+
         
         central_widget = QWidget()
         self.setCentralWidget(central_widget)
 
         self.setup_menu_bar()
-        #self.GUI_style()
         self.show()
 
         #menu bar and options
     def setup_menu_bar(self):
         menu_bar = self.menuBar()
+
+        # File menu
+        file_menu = menu_bar.addMenu("File")
+        file_menu.addAction("New")
+        file_menu.addAction("Open")
+        file_menu.addAction("Save")
+        file_menu.addSeparator()
+        file_menu.addAction("Exit", self.close)
+
 
         
 
